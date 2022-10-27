e174052a6705b1bec8724ae4f5911a2662823d75
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2022-10-27 16:49:44 +0600

    installed cors and initialized it

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 9000;
+
+app.use(cors());
 
 const courses = require('./data/courses.json');
 
